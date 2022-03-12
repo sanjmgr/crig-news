c90641bca71f5d6551c97e5911fa6a499fa2f395
sanjmgr <sanjmgr05@gmail.com>
2022-03-12 18:03:32 +0545

    Added content

---
+++ src/components/Carousel.vue
@@ -0,0 +1,142 @@
+<template>
+    <div id="default-carousel" data-carousel="slide" class="relative rounded-md bg-gray-500">
+        <!-- Carousel wrapper -->
+        <div class="relative overflow-hidden rounded-lg">
+            <!-- Item 1 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                <img
+                    src="https://cdn.pixabay.com/photo/2022/03/01/20/58/peace-genius-7042013_960_720.jpg"
+                    class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                    alt="..."
+                />
+            </div>
+
+            <!-- Item 2 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item="">
+                <img
+                    src="https://cdn.pixabay.com/photo/2022/02/15/00/40/lemonade-7014122_960_720.jpg"
+                    class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                    alt="..."
+                />
+            </div>
+
+            <!-- Item 3 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item="">
+                <img
+                    src="https://cdn.pixabay.com/photo/2021/12/22/02/29/motherhood-6886538_960_720.jpg"
+                    class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                    alt="..."
+                />
+            </div>
+
+            <!-- Item 4 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item="">
+                <img
+                    src="https://cdn.pixabay.com/photo/2020/03/26/10/58/norway-4970080_960_720.jpg"
+                    class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                    alt="..."
+                />
+            </div>
+
+            <!-- Item 5 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item="">
+                <img
+                    src="https://cdn.pixabay.com/photo/2019/03/18/04/53/bird-4062359_960_720.jpg"
+                    class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                    alt="..."
+                />
+            </div>
+        </div>
+
+        <!-- Slider indicators -->
+        <div class="absolute bottom-5 left-1/2 flex -translate-x-1/2 space-x-3">
+            <button
+                type="button"
+                class="h-3 w-3 rounded-full bg-white dark:bg-gray-800"
+                aria-current="true"
+                aria-label="Slide 1"
+                data-carousel-slide-to="0"
+            ></button>
+            <button
+                type="button"
+                class="h-3 w-3 rounded-full bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800"
+                aria-current="false"
+                aria-label="Slide 2"
+                data-carousel-slide-to="1"
+            ></button>
+            <button
+                type="button"
+                class="h-3 w-3 rounded-full bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800"
+                aria-current="false"
+                aria-label="Slide 3"
+                data-carousel-slide-to="2"
+            ></button>
+            <button
+                type="button"
+                class="h-3 w-3 rounded-full bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800"
+                aria-current="false"
+                aria-label="Slide 4"
+                data-carousel-slide-to="3"
+            ></button>
+            <button
+                type="button"
+                class="h-3 w-3 rounded-full bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800"
+                aria-current="false"
+                aria-label="Slide 5"
+                data-carousel-slide-to="4"
+            ></button>
+        </div>
+
+        <!-- Slider controls -->
+        <button
+            type="button"
+            class="group absolute top-0 left-0 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+            data-carousel-prev
+        >
+            <span
+                class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10"
+            >
+                <svg
+                    class="h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 19l-7-7 7-7"
+                    ></path>
+                </svg>
+                <span class="hidden">Previous</span>
+            </span>
+        </button>
+        <button
+            type="button"
+            class="group absolute top-0 right-0 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+            data-carousel-next
+        >
+            <span
+                class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10"
+            >
+                <svg
+                    class="h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5l7 7-7 7"
+                    ></path>
+                </svg>
+                <span class="hidden">Next</span>
+            </span>
+        </button>
+    </div>
+</template>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -3,31 +3,62 @@
         <div class="container mx-auto p-8 py-8 px-8 md:px-14">
             <div class="mt-4 grid grid-flow-col grid-rows-4 gap-4 md:grid-cols-12 lg:grid-rows-5">
                 <div
-                    class="grid place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-8 md:row-span-4 lg:col-span-6 lg:row-span-5"
+                    class="relative grid rounded-lg bg-white p-4 shadow-md md:col-span-8 md:row-span-4 lg:col-span-6 lg:row-span-5"
                 >
-                    01
+                    <Carousel />
                 </div>
 
                 <div
-                    class="grid place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-3"
+                    class="grid rounded-lg bg-white p-2 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-3"
                 >
-                    02
+                    <img
+                        class="h-full w-full rounded-md object-cover"
+                        src="https://bilder.bild.de/fotos/lieferprobleme-und-kunden-wut-hier-steckt-bei-samsung-der-wurm-drin-dce0e18ddfb144efba828ae07f4b0442-79273756/Bild/10.bild.jpg"
+                        alt=""
+                    />
+                    <h4 class="title px-2 pt-4 text-justify font-semibold leading-6">
+                        Lieferprobleme und Kunden-Wut: Hier steckt bei Samsung der Wurm drin - BILD
+                    </h4>
                 </div>
                 <div
-                    class="grid h-48 place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-2"
+                    class="grid h-48 rounded-lg bg-white p-2 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-2"
                 >
-                    03
+                    <img
+                        class="h-2/3 w-full rounded-md object-cover"
+                        src="https://imgix.gizmodo.com.au/content/uploads/sites/2/2022/03/11/dd083809a974ef2366146a3b780a2667-e1646952230362.gif?ar=16%3A9&auto=format&fit=crop&q=65&w=1200"
+                        alt=""
+                    />
+                    <p class="title -mt-14 px-2 text-justify font-semibold leading-4">
+                        Here's How Different Gran Turismo 7 Looks on PS4 Versus PS5 - Gizmodo
+                        Australia
+                    </p>
                 </div>
 
                 <div
-                    class="grid h-48 place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-3 md:row-span-2 md:hidden lg:grid"
+                    class="grid h-48 rounded-lg bg-white p-2 shadow-md md:col-span-3 md:row-span-2 md:hidden lg:grid"
                 >
-                    04
+                    <img
+                        class="h-2/3 w-full rounded-md object-cover"
+                        src="https://images.hindustantimes.com/tech/img/2022/03/12/1600x900/Untitled_design_(6)_1647061389520_1647061427293.jpg"
+                        alt=""
+                    />
+                    <p class="title -mt-14 px-2 text-justify font-semibold leading-4">
+                        iPhone SE 3 vs iPhone SE 2: Know the key differences before buying iPhone SE
+                        2022 - HT Tech
+                    </p>
                 </div>
                 <div
-                    class="hidden place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-3 md:row-span-3 lg:grid"
+                    class="hidden rounded-lg bg-white p-2 shadow-md md:col-span-3 md:row-span-3 lg:grid"
                 >
-                    05
+                    <img
+                        class="h-full w-full rounded-md object-cover"
+                        src="https://mm.netinfo.bg/branding/dbrand.php?p=aDE1UTNyWEtkWWZ3YWRpN09YV25aTld3Zy82RFBhbDhxTE5PZFlubzZna0tqMjRPdlMyNmVLM0Z6eGZraXM1SGprOE82cGVkdTAxS29VYjEvT0djNjFSdGdmSS85V3FBV2E3WDFueEVZa3hSV2pra2d4bVN6NUw3YTN2cERqbGh2UDIxS2czNVNXQlNSank4TWJMVjJzOERlNTZmNzVZOUpIL1E2bzllODFEazlYcVRhY0VqYW50a24zeFhleTh4"
+                        alt=""
+                    />
+                    <h4 class="title px-2 pt-4 text-justify font-semibold leading-6">
+                        Microsoft оправи \"бъг\" с личните данни в Windows - Vesti.bg - Новини от
+                        България и света
+                    </h4>
                 </div>
             </div>
             <div class="mt-4 grid grid-flow-row gap-4 md:grid-cols-12 md:grid-rows-6">
@@ -67,7 +98,10 @@
 </template>
 
 <script>
-export default {}
+import Carousel from './Carousel.vue'
+export default {
+    components: { Carousel },
+}
 </script>
 
 <style></style>
