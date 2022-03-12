e335e17c16782b3d08d96eeca1c2a9068caee4f2
sanjmgr <sanjmgr05@gmail.com>
2022-03-12 14:53:12 +0545

    Added base layout for landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="bg-gray-50">
+        <div class="container mx-auto p-8 py-8 px-8 md:px-14">
+            <div class="mt-4 grid grid-flow-col grid-rows-4 gap-4 md:grid-cols-12 lg:grid-rows-5">
+                <div
+                    class="grid place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-8 md:row-span-4 lg:col-span-6 lg:row-span-5"
+                >
+                    01
+                </div>
+
+                <div
+                    class="grid place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-3"
+                >
+                    02
+                </div>
+                <div
+                    class="grid h-48 place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-4 md:row-span-2 lg:col-span-3 lg:row-span-2"
+                >
+                    03
+                </div>
+
+                <div
+                    class="grid h-48 place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-3 md:row-span-2 md:hidden lg:grid"
+                >
+                    04
+                </div>
+                <div
+                    class="hidden place-content-center rounded-lg bg-white p-4 shadow-md md:col-span-3 md:row-span-3 lg:grid"
+                >
+                    05
+                </div>
+            </div>
+            <div class="mt-4 grid grid-flow-row gap-4 md:grid-cols-12 md:grid-rows-6">
+                <div
+                    class="col-span-4 row-span-3 grid place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    06
+                </div>
+                <div
+                    class="col-span-4 row-span-3 grid place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    07
+                </div>
+                <div
+                    class="col-span-4 row-span-3 grid place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    08
+                </div>
+                <div
+                    class="col-span-4 row-span-3 grid place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    09
+                </div>
+                <div
+                    class="col-span-4 row-span-3 grid place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    10
+                </div>
+                <div
+                    class="col-span-4 row-span-3 grid h-52 place-content-center rounded-lg bg-white p-4 shadow-md sm:h-48"
+                >
+                    11
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
